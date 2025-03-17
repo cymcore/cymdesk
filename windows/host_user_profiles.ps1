1556737e07d9d8b02e423610fbb708dee5b20920
ptimme01 <ptimme01@outlook.com>
2025-03-17 11:45:12 -0400

    feat(windows): added powershell update during firstlogon init script so stage2 scripts could run since it requires pwsh

--- windows/host_user_profiles.ps1
+++ windows/host_user_profiles.ps1
@@ -62,6 +62,7 @@ $init__windev__sysadmin = @{
     240 = { Install-Wsl -DistroName "Ubuntu-24.04" }
     300 = { $WingetFilePath = "C:\Users\$($env:USERNAME)\AppData\Local\Microsoft\WindowsApps\winget.exe" ; $counter = 0 ; while (!(test-path -path $WingetFilePath) -and ($counter -lt 30)) { Start-Sleep -Seconds 10 ; write-host $counter ; $counter++ } }
     301 = { Install-WingetApp -Id git.git -CustomArgs "--scope machine" }
+    302 = { Install-WingetApp -Id Microsoft.PowerShell -CustomArgs "--source winget" }
     800 = { powershell.exe -executionpolicy bypass -file $env:AUTOWINPATH\winauto.ps1 -action trigger ; powershell.exe -executionpolicy bypass -command "Start-Sleep -Seconds 10"}
     900 = { Restart-Computer -Force }
 }
