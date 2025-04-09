b53bd52ec9085941166319e02d0ba5849913621f
ptimme01 <ptimme01@outlook.com>
2025-04-09 09:47:06 -0400

    feat(hyper-v-deploy-windows): added two features, first space key to continue and second the countdown on one line so message doesn't scroll off screen

--- hyper-v/deploy-windows/utils_pshelper.ps1
+++ hyper-v/deploy-windows/utils_pshelper.ps1
@@ -2,18 +2,22 @@ function Show-OptionalUserExitAndContinue {
     [CmdletBinding()]
     param (
         [string]$Message = "Warning",
-        [Int64]$timeout = 11
+        [Int64]$timeout = 10
     )
 
     # Display the warning message in red
 
     Write-Host $Message -ForegroundColor Red
-    Write-Host "Press the enter key to exit program or wait 10 seconds to continue."
+    Write-Host "Press the ENTER key to exit program, SPACE key to continue, or wait $timeout seconds to automatically continue."
 
     # Check for the Enter key in a loop
     while ((++$counter) -lt $timeout) {
+        # Countdown logic and display)
+        if ($($counter) -eq 1) { write-host("$timeout ") -NoNewline } # Downside of initialization of counter in the while loop declaration
         Start-Sleep -Seconds 1
-        write-host "$($timeout - $counter) seconds left before continuing..."
+        write-host "$($timeout - $counter)  " -NoNewline
+        if ($($timeout - $counter) -eq 1) { write-host("`r") }
+
         if ([Console]::KeyAvailable) {
             # A key was pressed, check if it's the Enter key
             $Key = [Console]::ReadKey($true).Key
@@ -21,6 +25,10 @@ function Show-OptionalUserExitAndContinue {
             if ($Key -eq [ConsoleKey]::Enter) {
                 Write-Host "Enter key pressed, quiting execution..."
                 Throw "User requested exit"
+            }
+            if ($Key -eq [ConsoleKey]::Spacebar) {
+                Write-Host "Space key pressed, continue execution..."
+                return
             }
         }
     }
