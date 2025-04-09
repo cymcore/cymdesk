6a0bca39b559f2d61a4f61df9359d143780ae740
ptimme01 <ptimme01@outlook.com>
2025-04-09 11:32:23 -0400

    feat(hyper-v-deploy-windows): added a new keypress to continue function and made colors across the show message functions
    rename function which would have been breaking change but that function no in production yet

--- hyper-v/deploy-windows/utils_pshelper.ps1
+++ hyper-v/deploy-windows/utils_pshelper.ps1
@@ -1,13 +1,15 @@
 function Show-OptionalUserExitAndContinue {
     [CmdletBinding()]
     param (
-        [string]$Message = "Warning",
-        [Int64]$timeout = 10
+        [string]$Message = "Message",
+        [Int64]$timeout = 10,
+        [ValidateSet("Green", "Yellow", "Red")]
+        [string]$color = "Yellow"
     )
 
     # Display the warning message in red
 
-    Write-Host $Message -ForegroundColor Red
+    Write-Host $Message -ForegroundColor $color
     Write-Host "Press the ENTER key to exit program, SPACE key to continue, or wait $timeout seconds to automatically continue."
 
     # Check for the Enter key in a loop
@@ -19,22 +21,24 @@ function Show-OptionalUserExitAndContinue {
         if ($($timeout - $counter) -eq 1) { write-host("`r") }
 
         if ([Console]::KeyAvailable) {
-            # A key was pressed, check if it's the Enter key
+            # A key was pressed, check if it's the Enter and Spacebar key
             $Key = [Console]::ReadKey($true).Key
 
             if ($Key -eq [ConsoleKey]::Enter) {
-                Write-Host "Enter key pressed, quiting execution..."
+                write-host("`r")
+                Write-Host "ENTER key pressed, quiting execution..."
                 Throw "User requested exit"
             }
             if ($Key -eq [ConsoleKey]::Spacebar) {
-                Write-Host "Space key pressed, continue execution..."
+                write-host("`r")
+                Write-Host "SPACE key pressed, continue execution..."
                 return
             }
         }
     }
 }
 
-function Show-Countdown {
+function Show-OptionalUserCountdownContinue {
     [CmdletBinding()]
     param (
         [string]$Message = "Message",
@@ -43,7 +47,7 @@ function Show-Countdown {
         [string]$color = "Yellow"
     )
     Write-Host $Message -ForegroundColor $color
-    Write-Host "Continuing in: " -ForegroundColor $color -NoNewline
+    Write-Host "Press SPACE to continue (or wait for): " -ForegroundColor $color -NoNewline
    
     $counter = 0
     while (($counter) -lt $timeout) {
@@ -52,5 +56,26 @@ function Show-Countdown {
         if ($($timeout - $counter) -eq 1) { write-host("`r") }
         Start-Sleep -Seconds 1
         $counter++
+
+        if ([Console]::KeyAvailable) {
+            # A key was pressed, check if it's the spacebar key
+            $Key = [Console]::ReadKey($true).Key
+            if ($Key -eq [ConsoleKey]::Spacebar) {
+                write-host("`r")
+                Write-Host "SPACE key pressed, continue execution..."
+                return
+            }
+        }
     }
+}
+
+function Show-UserKeyPressToContinue {
+    param (
+        [string]$Message = "Message",
+        [ValidateSet("Green", "Yellow", "Red")]
+        [string]$color = "Yellow"
+    )
+    Write-Host $Message -ForegroundColor $color
+    Write-Host "Press any key to continue." -ForegroundColor $color -NoNewline
+    [void][System.Console]::ReadKey($true)
 }
