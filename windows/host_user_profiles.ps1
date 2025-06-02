40d91b4f62d0d2e2b1b62b7597561a9ead57f74a
ptimme01 <ptimme01@outlook.com>
2025-06-02 15:55:23 -0400

    feat(windows): added dotnet sdk 8 to ptimme01

--- windows/host_user_profiles.ps1
+++ windows/host_user_profiles.ps1
@@ -120,6 +120,7 @@ $windev__ptimme01 = @{
 $north__ptimme01 = @{
     220 = { Add-InitialGitConfig -UserName "ptimme01" -UserEmail "ptimme01@outlook.com" }
     300 = { Install-WingetApp -Id microsoft.visualstudiocode -CustomArgs "--override ""/VERYSILENT /SP- /MERGETASKS='!runcode,!desktopicon,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath\'""" }
+    330 = { Install-WingetApp -Id Microsoft.DotNet.SDK.8 }
     340 = { Install-WslDistribution -DistroName "Ubuntu-24.04" -Name main }
     345 = { Copy-GitRepo -GitRepoUrl "https://github.com/cymcore/cymdesk.git" -DestinationPath "C:\xfer\cymdesk" }
     350 = { Set-WslInstanceConfiguration -Name main -UserName ptimme01 -InstanceCymdeskPath "/mnt/c/xfer/cymdesk" }
